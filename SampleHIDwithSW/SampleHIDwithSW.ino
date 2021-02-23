616d1e2cfcff10261e7202ff6aa4331c67298ecb
Va1kar1e <siwanont1@gmail.com>
2021-02-23 21:00:15 +0700

    Update SampleHIDwithSW.ino

--- SampleHIDwithSW/SampleHIDwithSW.ino
+++ SampleHIDwithSW/SampleHIDwithSW.ino
@@ -9,11 +9,7 @@ char *myTexts[] = {
   "This is string from A02",
   "This is string from A03",
   "This is string from A04",   
-  "This is string from A05", 
-  "This is string from A06", 
-  "This is string from A07",
-  "This is string from A08",
-  "This is string from A09"
+  "This is string from A05"
 };
 
 int i = 0;
@@ -29,14 +25,13 @@ void setup() {
 
 void loop() {
   blinkState();
-  delay(500);
   if (digitalRead(inputPin) == 0) {
     DigiKeyboard.sendKeyStroke(0);
     DigiKeyboard.println(myTexts[i]);
     DigiKeyboard.delay(1000);  
   }
  
-  if (i >= 9) {
+  if (i >= 5) {
     i = 0;
     delay(1000);
     blinkIntro();
@@ -61,5 +56,5 @@ void blinkState() {
   digitalWrite(outputPin,HIGH);
   delay(500);
   digitalWrite(outputPin,LOW);
-  delay(500);
+  delay(1000);
 }
